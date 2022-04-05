0b8ed628ee984e78fb39f8dfb2dfc4f639723079
appmonster007 <prathitlenovoa1000@gmail.com>
2022-04-06 02:43:06 +0530

    corrected odxt 'server'

--- odxt/odxt_client.py
+++ odxt/odxt_client.py
@@ -1,16 +1,20 @@
 from xmlrpc.client import MAXINT
-import pickle, socket, sys, random
-from util.constants import HOST,PORT
+import pickle
+import socket
+import sys
+import random
+from util.constants import HOST, PORT
 from util import dsse_util
 
+
 class odxt_client:
-    def __init__(self, socket_conn):
+    def __init__(self, addr):
         self.sk: tuple = ()
         self.st: dict = None
         self.p: int = -1
         self.g: int = -1
-        self.conn = socket_conn
-    
+        self.addr = addr
+
     # ODXT Setup(λ)
     def Setup(self, λ):
         # self.p = number.getPrime(16)
@@ -24,49 +28,59 @@ class odxt_client:
         # self.p = 9803877828113247241079792513194491218341545278043756244841606553497839645277744524007466705683349057071449190848792221929552903517949301195746698367877099
         # self.p = 20963
         # self.g = 65537
-        
+
         Kt = dsse_util.gen_key_F(λ)
         Kx = dsse_util.gen_key_F(λ)
         Ky = dsse_util.gen_key_F(λ)
         Kz = dsse_util.gen_key_F(λ)
-        UpdateCnt, Tset, XSet = dict(),dict(), set()
+        UpdateCnt, Tset, XSet = dict(), dict(), set()
         self.sk, self.st = (Kt, Kx, Ky, Kz), UpdateCnt
         EDB = (Tset, XSet)
-        self.conn.send(pickle.dumps((0,(EDB, self.p))))
-        if(pickle.loads(self.conn.recv(4096))==(1,)):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(self.addr)
+        conn.send(pickle.dumps((0, (EDB, self.p))))
+        data = pickle.loads(conn.recv(4096))
+        print(f"setup{data}")
+        if(data == (1,)):
             print("Setup completed")
-    
+        conn.close()
+
     def Update(self, op: str, id_w_tuple):
-        id,w = id_w_tuple
+        id, w = id_w_tuple
         Kt, Kx, Ky, Kz = self.sk
         if(not w in self.st):
-            self.st[w]=0
-        self.st[w]+=1
+            self.st[w] = 0
+        self.st[w] += 1
         w_wc = str(w)+str(self.st[w])
-        addr = dsse_util.prf_F(Kt,(w_wc+str(0)).encode())
+        addr = dsse_util.prf_F(Kt, (w_wc+str(0)).encode())
         b1 = (str(op)+str(id)).encode()
-        b2 = dsse_util.prf_F(Kt,(w_wc+str(1)).encode())
+        b2 = dsse_util.prf_F(Kt, (w_wc+str(1)).encode())
         val = dsse_util.bytes_XOR(b1, b2)
         A0 = dsse_util.prf_Fp(Ky, b1, self.p, self.g)
         A = int.from_bytes(A0, 'little')
-        B0 = dsse_util.prf_Fp(Kz,(w_wc).encode(), self.p, self.g)
+        B0 = dsse_util.prf_Fp(Kz, (w_wc).encode(), self.p, self.g)
         B = int.from_bytes(B0, 'little')
         B_inv = dsse_util.mul_inv(B, self.p-1)
         C0 = dsse_util.prf_Fp(Kx, str(w).encode(), self.p, self.g)
         C = int.from_bytes(C0, 'little')
         α = (A*B_inv)
         xtag = pow(self.g, C*A, self.p)
-        self.conn.send(pickle.dumps((1,(addr, val, α, xtag))))
-        if(pickle.loads(self.conn.recv(4096))==(1,)):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(self.addr)
+        conn.send(pickle.dumps((1, (addr, val, α, xtag))))
+        data = pickle.loads(conn.recv(1024))
+        print(f"setup{data}")
+        if(data == (1,)):
             print("Update completed")
-    
-    def Search(self,q):
+        conn.close()
+
+    def Search(self, q):
         n = len(q)
         Kt, Kx, Ky, Kz = self.sk
         # min_uc = np.argmin(np.array(list(self.st.values())))
         # w1_uc = list(self.st.values())[min_uc]
         # w1 = list(self.st.keys())[min_uc]
-    
+
         w1_uc = MAXINT
         w1 = ""
         for x in q:
@@ -77,71 +91,84 @@ class odxt_client:
         xtokenlists = []
         if(w1 in self.st):
             for j in range(w1_uc):
-                saddr_j = dsse_util.prf_F(Kt, (str(w1)+str(j+1)+str(0)).encode())
+                saddr_j = dsse_util.prf_F(
+                    Kt, (str(w1)+str(j+1)+str(0)).encode())
                 stokenlist.append(saddr_j)
                 xtl = []
-                B0 = dsse_util.prf_Fp(Kz,(str(w1)+str(j+1)).encode(), self.p, self.g)
+                B0 = dsse_util.prf_Fp(
+                    Kz, (str(w1)+str(j+1)).encode(), self.p, self.g)
                 B = int.from_bytes(B0, 'little')
                 for i in range(n):
                     if(q[i] != w1):
-                        A0 = dsse_util.prf_Fp(Kx,(str(q[i])).encode(), self.p, self.g)
+                        A0 = dsse_util.prf_Fp(
+                            Kx, (str(q[i])).encode(), self.p, self.g)
                         A = int.from_bytes(A0, 'little')
                         xtoken = pow(self.g, A*B, self.p)
                         xtl.append(xtoken)
                 random.shuffle(xtl)
                 xtokenlists.append(xtl)
         res = (stokenlist, xtokenlists)
-        self.conn.send(pickle.dumps((2,res)))
-            
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(self.addr)
+        conn.send(pickle.dumps((2, res)))
+
         #
-        #SERVER WORK
+        # SERVER WORK
         #
-        resp_tup = pickle.loads(self.conn.recv(4096))
+
+        resp_tup = pickle.loads(conn.recv(4096))
         sEOpList = resp_tup[0]
         IdList = []
         for l in sEOpList:
-            j,sval,cnt = l
+            j, sval, cnt = l
             X0 = dsse_util.prf_F(Kt, (str(w1)+str(j+1)+str(1)).encode())
             op_id = dsse_util.bytes_XOR(sval, X0)
             op_id = op_id.decode().rstrip('\x00')
-            if(op_id[:3]=='add' and cnt==n):
+            if(op_id[:3] == 'add' and cnt == n):
                 IdList.append(int(op_id[3:]))
-            elif(op_id[:3]=='del' and cnt>0 and int(op_id[3:]) in IdList):
+            elif(op_id[:3] == 'del' and cnt > 0 and int(op_id[3:]) in IdList):
                 IdList.remove(int(op_id[3:]))
         print(list(set(IdList)))
+        conn.close()
         return list(set(IdList))
+
+    def close_server(self):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect(self.addr)
+        conn.send(pickle.dumps(("q",)))
+        conn.close()
 
 
 if __name__ == "__main__":
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    client_obj = odxt_client(s)
+    # HOST = 'localhost'
+    # PORT = 50060
+    client_obj = odxt_client((HOST, PORT))
     client_obj.Setup(100)
     # print(client_obj.sk,client_obj.st)
 
-    client_obj.Update('add',(2,"apple"))
-    client_obj.Update('add',(4,"apple"))
-    client_obj.Update('add',(5,"apple"))
-    client_obj.Update('add',(6,"apple"))
-    client_obj.Update('add',(7,"apple"))
-    client_obj.Update('add',(8,"apple"))
-    client_obj.Update('del',(7,"apple"))
+    client_obj.Update('add', (2, "apple"))
+    client_obj.Update('add', (4, "apple"))
+    client_obj.Update('add', (5, "apple"))
+    client_obj.Update('add', (6, "apple"))
+    client_obj.Update('add', (7, "apple"))
+    client_obj.Update('add', (8, "apple"))
+    client_obj.Update('del', (7, "apple"))
 
-    client_obj.Update('add',(3,"banana"))
-    client_obj.Update('add',(4,"banana"))
-    client_obj.Update('add',(5,"banana"))
-    client_obj.Update('add',(6,"banana"))
-    client_obj.Update('add',(7,"banana"))
-    client_obj.Update('del',(4,"banana"))
+    client_obj.Update('add', (3, "banana"))
+    client_obj.Update('add', (4, "banana"))
+    client_obj.Update('add', (5, "banana"))
+    client_obj.Update('add', (6, "banana"))
+    client_obj.Update('add', (7, "banana"))
+    client_obj.Update('del', (4, "banana"))
 
-    client_obj.Update('add',(3,"pincode"))
-    client_obj.Update('add',(4,"pincode"))
-    client_obj.Update('add',(5,"pincode"))
-    client_obj.Update('add',(6,"pincode"))
-    client_obj.Update('add',(7,"pincode"))
-    client_obj.Update('del',(3,"pincode"))
+    client_obj.Update('add', (3, "pincode"))
+    client_obj.Update('add', (4, "pincode"))
+    client_obj.Update('add', (5, "pincode"))
+    client_obj.Update('add', (6, "pincode"))
+    client_obj.Update('add', (7, "pincode"))
+    client_obj.Update('del', (3, "pincode"))
     # print("Search for apple")
     # client_obj.Search(["apple"])
     # print("Search for banana")
@@ -154,6 +181,3 @@ if __name__ == "__main__":
     client_obj.Search(["apple", "pincode"])
     print("Search for apple and pincode and banana")
     client_obj.Search(["apple", "pincode", "banana"])
-    client_obj.conn.send(pickle.dumps(("q",)))
-    s.close()
-    
--- odxt/odxt_server.py
+++ odxt/odxt_server.py
@@ -1,40 +1,50 @@
-import pickle, sys
-import socket
-from util.constants import HOST,PORT
+import pickle
+import sys
+import logging
+import socketserver
+from util.constants import HOST, PORT
 
-class odxt_server:
-    def __init__(self, socket_tup) -> None:
+logging.basicConfig(level=logging.INFO)
+
+
+class serverReqHandler(socketserver.BaseRequestHandler):
+    def __init__(self, request, addr, server):
+        super().__init__(request, addr, server)
+
+    def handle(self):
+        resp_tup = pickle.loads(self.request.recv(4096))
+        if(resp_tup[0] == 0):  # for setup
+            self.server.Setup(resp_tup[1])
+            data = (1,)
+            logging.debug("setup completed")
+        elif(resp_tup[0] == 1):
+            self.server.Update(resp_tup[1])
+            data = (1,)
+            logging.debug("update completed")
+        elif(resp_tup[0] == 2):
+            data = self.server.Search(resp_tup[1])
+            logging.debug("search completed")
+
+        self.request.sendall(pickle.dumps(data))
+        logging.debug('handled')
+
+
+class odxt_server(socketserver.TCPServer):
+    def __init__(self, addr, handler_class=serverReqHandler) -> None:
         self.EDB = None
         self.p = -1
-        self.conn,self.sock_addr = socket_tup
+        super().__init__(addr, handler_class)
 
-    def Run(self):
-        resp_tup = pickle.loads(self.conn.recv(4096))
-        if(resp_tup[0]==0):#for setup
-            self.Setup(resp_tup[1])
-            self.conn.send(pickle.dumps((1,)))
-            return 1
-        elif(resp_tup[0]==1):
-            self.Update(resp_tup[1])
-            self.conn.send(pickle.dumps((1,)))
-            return 1
-        elif(resp_tup[0]==2):
-            self.Search(resp_tup[1])
-            return 1
-        elif(resp_tup[0]=="q"):
-            self.conn.close()
-            return 0
-            
-    def Setup(self,res):
+    def Setup(self, res):
         self.EDB, self.p = res
-        
-    def Update(self,avax_tup):
+
+    def Update(self, avax_tup):
         TSet, XSet = self.EDB
-        addr,val,α,xtag = avax_tup
-        TSet[addr]=(val,α)
+        addr, val, α, xtag = avax_tup
+        TSet[addr] = (val, α)
         XSet.add(xtag)
         self.EDB = (TSet, XSet)
-    
+
     def Search(self, tknlists):
         TSet, XSet = self.EDB
         stokenlist = tknlists[0]
@@ -48,27 +58,13 @@ class odxt_server:
                 xtoken_ij = xt
                 xtag_ij = pow(xtoken_ij, α, self.p)
                 if(xtag_ij in XSet):
-                    cnt+=1
-            sEOpList.append((j,sval,cnt))
-        self.conn.send(pickle.dumps((sEOpList,)))
-    
+                    cnt += 1
+            sEOpList.append((j, sval, cnt))
+        return (sEOpList,)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(1)
-
-    conn, addr = s.accept()
-    # print('Connected by', addr)
-    server_obj = odxt_server((conn,addr))
-    server_obj.Run()
-    # print("new edb recieved to server: ",server_obj.EDB)
-    server_obj.Run()
-    # print(server_obj.EDB)
-    run = 1
-    while(run):
-        run = server_obj.Run()
-    # conn.close()
+    server = odxt_server((HOST, PORT), serverReqHandler)
+    server.serve_forever()
